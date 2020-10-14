22ac6557cd6b66fd256e9719be59b85aa6d3d21f
Guilherme Gentile <guigass@live.co.uk>
2020-10-14 10:19:55 -0300

    Ajuste na tipagem das propriedades

--- SharpClickEntregas.API/Models/Order.cs
+++ SharpClickEntregas.API/Models/Order.cs
@@ -8,105 +8,105 @@ namespace SharpClickEntregas.API.Models
     public partial class Order
     {
         [JsonProperty("order_id")]
-        public object OrderId { get; set; }
+        public long OrderId { get; set; }
 
         [JsonProperty("order_name")]
-        public object OrderName { get; set; }
+        public string OrderName { get; set; }
 
-        [JsonProperty("vehicle_type_id")]
-        public long VehicleTypeId { get; set; }
+        [JsonProperty("vehicle_type_id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? VehicleTypeId { get; set; }
 
-        [JsonProperty("created_datetime")]
-        public DateTimeOffset CreatedDatetime { get; set; }
+        [JsonProperty("created_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? CreatedDatetime { get; set; }
 
-        [JsonProperty("finish_datetime")]
-        public object FinishDatetime { get; set; }
+        [JsonProperty("finish_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? FinishDatetime { get; set; }
 
-        [JsonProperty("status")]
+        [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
         public string Status { get; set; }
 
-        [JsonProperty("status_description")]
+        [JsonProperty("status_description", NullValueHandling = NullValueHandling.Ignore)]
         public string StatusDescription { get; set; }
 
-        [JsonProperty("matter")]
+        [JsonProperty("matter", NullValueHandling = NullValueHandling.Ignore)]
         public string Matter { get; set; }
 
-        [JsonProperty("total_weight_kg")]
-        public long TotalWeightKg { get; set; }
+        [JsonProperty("total_weight_kg", NullValueHandling = NullValueHandling.Ignore)]
+        public long? TotalWeightKg { get; set; }
 
-        [JsonProperty("is_client_notification_enabled")]
-        public bool IsClientNotificationEnabled { get; set; }
+        [JsonProperty("is_client_notification_enabled", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsClientNotificationEnabled { get; set; }
 
-        [JsonProperty("is_contact_person_notification_enabled")]
-        public bool IsContactPersonNotificationEnabled { get; set; }
+        [JsonProperty("is_contact_person_notification_enabled", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsContactPersonNotificationEnabled { get; set; }
 
-        [JsonProperty("loaders_count")]
-        public long LoadersCount { get; set; }
+        [JsonProperty("loaders_count", NullValueHandling = NullValueHandling.Ignore)]
+        public long? LoadersCount { get; set; }
 
-        [JsonProperty("backpayment_details")]
-        public object BackpaymentDetails { get; set; }
+        [JsonProperty("backpayment_details", NullValueHandling = NullValueHandling.Ignore)]
+        public string BackpaymentDetails { get; set; }
 
-        [JsonProperty("points")]
+        [JsonProperty("points", NullValueHandling = NullValueHandling.Ignore)]
         public List<OrderPoint> Points { get; set; }
 
-        [JsonProperty("payment_amount")]
+        [JsonProperty("payment_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string PaymentAmount { get; set; }
 
-        [JsonProperty("delivery_fee_amount")]
+        [JsonProperty("delivery_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string DeliveryFeeAmount { get; set; }
 
-        [JsonProperty("intercity_delivery_fee_amount")]
+        [JsonProperty("intercity_delivery_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string IntercityDeliveryFeeAmount { get; set; }
 
-        [JsonProperty("weight_fee_amount")]
+        [JsonProperty("weight_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string WeightFeeAmount { get; set; }
 
-        [JsonProperty("insurance_amount")]
+        [JsonProperty("insurance_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string InsuranceAmount { get; set; }
 
-        [JsonProperty("insurance_fee_amount")]
+        [JsonProperty("insurance_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string InsuranceFeeAmount { get; set; }
 
-        [JsonProperty("loading_fee_amount")]
+        [JsonProperty("loading_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string LoadingFeeAmount { get; set; }
 
-        [JsonProperty("money_transfer_fee_amount")]
+        [JsonProperty("money_transfer_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string MoneyTransferFeeAmount { get; set; }
 
-        [JsonProperty("suburban_delivery_fee_amount")]
+        [JsonProperty("suburban_delivery_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string SuburbanDeliveryFeeAmount { get; set; }
 
-        [JsonProperty("overnight_fee_amount")]
+        [JsonProperty("overnight_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string OvernightFeeAmount { get; set; }
 
-        [JsonProperty("discount_amount")]
+        [JsonProperty("discount_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string DiscountAmount { get; set; }
 
-        [JsonProperty("backpayment_amount")]
+        [JsonProperty("backpayment_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string BackpaymentAmount { get; set; }
 
-        [JsonProperty("cod_fee_amount")]
+        [JsonProperty("cod_fee_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string CodFeeAmount { get; set; }
 
-        [JsonProperty("backpayment_photo_url")]
-        public object BackpaymentPhotoUrl { get; set; }
+        [JsonProperty("backpayment_photo_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string BackpaymentPhotoUrl { get; set; }
 
-        [JsonProperty("itinerary_document_url")]
-        public object ItineraryDocumentUrl { get; set; }
+        [JsonProperty("itinerary_document_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string ItineraryDocumentUrl { get; set; }
 
-        [JsonProperty("waybill_document_url")]
-        public object WaybillDocumentUrl { get; set; }
+        [JsonProperty("waybill_document_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string WaybillDocumentUrl { get; set; }
 
         [JsonProperty("courier")]
         public object Courier { get; set; }
 
-        [JsonProperty("is_motobox_required")]
-        public bool IsMotoboxRequired { get; set; }
+        [JsonProperty("is_motobox_required", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsMotoboxRequired { get; set; }
 
-        [JsonProperty("payment_method")]
+        [JsonProperty("payment_method", NullValueHandling = NullValueHandling.Ignore)]
         public string PaymentMethod { get; set; }
 
-        [JsonProperty("bank_card_id")]
-        public object BankCardId { get; set; }
+        [JsonProperty("bank_card_id", NullValueHandling = NullValueHandling.Ignore)]
+        public int? BankCardId { get; set; }
     }
 }
--- SharpClickEntregas.API/Models/PointResponse.cs
+++ SharpClickEntregas.API/Models/PointResponse.cs
@@ -7,77 +7,77 @@ namespace SharpClickEntregas.API.Models
 {
     public partial class PointResponse
     {
-        [JsonProperty("point_type")]
+        [JsonProperty("point_type", NullValueHandling = NullValueHandling.Ignore)]
         public string PointType { get; set; }
 
-        [JsonProperty("point_id")]
-        public long PointId { get; set; }
+        [JsonProperty("point_id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? PointId { get; set; }
 
-        [JsonProperty("delivery_id")]
+        [JsonProperty("delivery_id", NullValueHandling = NullValueHandling.Ignore)]
         public long? DeliveryId { get; set; }
 
-        [JsonProperty("client_order_id")]
-        public object ClientOrderId { get; set; }
+        [JsonProperty("client_order_id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ClientOrderId { get; set; }
 
-        [JsonProperty("address")]
+        [JsonProperty("address", NullValueHandling = NullValueHandling.Ignore)]
         public string Address { get; set; }
 
-        [JsonProperty("latitude")]
+        [JsonProperty("latitude", NullValueHandling = NullValueHandling.Ignore)]
         public string Latitude { get; set; }
 
-        [JsonProperty("longitude")]
+        [JsonProperty("longitude", NullValueHandling = NullValueHandling.Ignore)]
         public string Longitude { get; set; }
 
-        [JsonProperty("required_start_datetime")]
-        public DateTimeOffset RequiredStartDatetime { get; set; }
+        [JsonProperty("required_start_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? RequiredStartDatetime { get; set; }
 
-        [JsonProperty("required_finish_datetime")]
-        public DateTimeOffset RequiredFinishDatetime { get; set; }
+        [JsonProperty("required_finish_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? RequiredFinishDatetime { get; set; }
 
-        [JsonProperty("arrival_start_datetime")]
-        public object ArrivalStartDatetime { get; set; }
+        [JsonProperty("arrival_start_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset ArrivalStartDatetime { get; set; }
 
-        [JsonProperty("arrival_finish_datetime")]
-        public object ArrivalFinishDatetime { get; set; }
+        [JsonProperty("arrival_finish_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset ArrivalFinishDatetime { get; set; }
 
-        [JsonProperty("estimated_arrival_datetime")]
-        public object EstimatedArrivalDatetime { get; set; }
+        [JsonProperty("estimated_arrival_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset EstimatedArrivalDatetime { get; set; }
 
-        [JsonProperty("courier_visit_datetime")]
-        public object CourierVisitDatetime { get; set; }
+        [JsonProperty("courier_visit_datetime", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset CourierVisitDatetime { get; set; }
 
-        [JsonProperty("contact_person")]
+        [JsonProperty("contact_person", NullValueHandling = NullValueHandling.Ignore)]
         public ContactPerson ContactPerson { get; set; }
 
-        [JsonProperty("taking_amount")]
+        [JsonProperty("taking_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string TakingAmount { get; set; }
 
-        [JsonProperty("buyout_amount")]
+        [JsonProperty("buyout_amount", NullValueHandling = NullValueHandling.Ignore)]
         public string BuyoutAmount { get; set; }
 
-        [JsonProperty("note")]
-        public object Note { get; set; }
+        [JsonProperty("note", NullValueHandling = NullValueHandling.Ignore)]
+        public string Note { get; set; }
 
-        [JsonProperty("packages")]
+        [JsonProperty("packages", NullValueHandling = NullValueHandling.Ignore)]
         public List<object> Packages { get; set; }
 
-        [JsonProperty("is_cod_cash_voucher_required")]
-        public bool IsCodCashVoucherRequired { get; set; }
+        [JsonProperty("is_cod_cash_voucher_required", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsCodCashVoucherRequired { get; set; }
 
-        [JsonProperty("place_photo_url")]
-        public object PlacePhotoUrl { get; set; }
+        [JsonProperty("place_photo_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string PlacePhotoUrl { get; set; }
 
-        [JsonProperty("sign_photo_url")]
-        public object SignPhotoUrl { get; set; }
+        [JsonProperty("sign_photo_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string SignPhotoUrl { get; set; }
 
-        [JsonProperty("tracking_url")]
-        public Uri TrackingUrl { get; set; }
+        [JsonProperty("tracking_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string TrackingUrl { get; set; }
 
-        [JsonProperty("checkin")]
+        [JsonProperty("checkin", NullValueHandling = NullValueHandling.Ignore)]
         public object Checkin { get; set; }
 
-        [JsonProperty("is_order_payment_here")]
-        public bool IsOrderPaymentHere { get; set; }
+        [JsonProperty("is_order_payment_here", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsOrderPaymentHere { get; set; }
 
         [JsonProperty("building_number")]
         public object BuildingNumber { get; set; }
